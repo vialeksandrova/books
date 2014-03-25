9d0e923a50e4dd01b96cb6c3f5198a50468b0385
vi aleksandrova <viialeksandrova@gmail.com>
2014-03-25 23:21:36 +0200

    Update books.sql

--- books.sql
+++ books.sql
@@ -1,26 +1,17 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.11.1deb1
+-- version 3.2.0.1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 09, 2013 at 12:49 PM
--- Server version: 5.5.32
--- PHP Version: 5.4.6-1ubuntu1.4
+-- Generation Time: Mar 25, 2014 at 11:20 
+-- Server version: 5.1.36
+-- PHP Version: 5.3.0
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `books`
 --
-CREATE DATABASE `books` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `books`;
 
 -- --------------------------------------------------------
 
@@ -32,7 +23,16 @@ CREATE TABLE IF NOT EXISTS `authors` (
   `author_id` int(11) NOT NULL AUTO_INCREMENT,
   `author_name` varchar(250) NOT NULL,
   PRIMARY KEY (`author_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `authors`
+--
+
+INSERT INTO `authors` (`author_id`, `author_name`) VALUES
+(1, 'адамс'),
+(2, 'пратчет'),
+(4, 'иванов');
 
 -- --------------------------------------------------------
 
@@ -44,7 +44,19 @@ CREATE TABLE IF NOT EXISTS `books` (
   `book_id` int(11) NOT NULL AUTO_INCREMENT,
   `book_title` varchar(250) NOT NULL,
   PRIMARY KEY (`book_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8  ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `books`
+--
+
+INSERT INTO `books` (`book_id`, `book_title`) VALUES
+(1, 'kotka'),
+(2, 'котка мотка'),
+(3, 'котка мотка'),
+(4, 'никой'),
+(5, 'ggg'),
+(6, 'книга много');
 
 -- --------------------------------------------------------
 
@@ -59,6 +71,41 @@ CREATE TABLE IF NOT EXISTS `books_authors` (
   KEY `author_id` (`author_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--
+-- Dumping data for table `books_authors`
+--
+
+INSERT INTO `books_authors` (`book_id`, `author_id`) VALUES
+(3, 4),
+(4, 1),
+(5, 2),
+(6, 1),
+(6, 2),
+(6, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=15 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES
+(1, 'vihra', '123456'),
+(2, 'joro', '666666'),
+(3, 'ivan', 'lala'),
+(14, 'user1', 'password'),
+(6, 'maya', 'dddd'),
+(7, 'mishel', 'bebe'),
+(8, 'kotka', 'motka'),
+(13, 'mechka', 'bechka');
